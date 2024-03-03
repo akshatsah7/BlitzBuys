fa07de8f7f3cab44e78d992f4fff8dee549c72e1
akshatsah7 <akshatsah63@gmail.com>
2024-03-03 20:54:00 +0530

    first commit

---
+++ src/components/Data.jsx
@@ -0,0 +1,106 @@
+export const items = [
+  {
+    id: 1,
+    category: "mobiles",
+    title: "Apple iPhone 14",
+    imgSrc: "https://m.media-amazon.com/images/I/61BGE6iu4AL._SL1500_.jpg",
+    amazonLink: "https://amzn.to/3PuckZp",
+    description: "Apple iPhone 14 Plus (512 GB) - Blue",
+    brief_det:
+      "Immerse yourself in the brilliance of the 16.95 cm (6.7-inch) Super Retina XDR display of the latest smartphone, where every detail comes to life with stunning clarity and vibrancy. Capture breathtaking photos in any lighting condition with the advanced camera system, ensuring exceptional image quality with every shot. Explore the new frontier of cinematography with cinematic mode now available in 4K Dolby Vision at up to 30 fps, allowing you to create cinematic masterpieces right from your device. Experience smooth and steady handheld videos with the action mode feature. Stay safe with vital safety technology like Crash Detection, which automatically calls for help when needed. Enjoy uninterrupted usage with all-day battery life and up to 26 hours of video playback. With industry-leading durability features including Ceramic Shield and water resistance, this device is built to withstand the rigors of everyday life while delivering unparalleled performance and reliability.",
+    price: "101900",
+  },
+  {
+    id: 5,
+    category: "laptops",
+    title: "Xiaomi Mi Notebook Ultra",
+    imgSrc: "https://m.media-amazon.com/images/I/710aJMxsUXL._SX679_.jpg",
+    amazonLink: "https://amzn.to/3EzeQaC",
+    description:
+      "Xiaomi Mi Notebook Ultra Max- Intel 11th Gen i5-11320H Thin & Light (Intel Iris Xe Graphics/16GB DDR4 RAM/512GB SSD/15.6",
+    brief_det:
+      "Experience seamless performance and exceptional productivity with the latest laptop featuring an 11th Gen Intel Tiger Lake Core i5-11320H processor, boasting speeds of up to 4.4 GHz. With 4 cores, 8 threads, and 8 MB cache, it effortlessly handles demanding tasks with ease. Immerse yourself in stunning visuals on the 39.62 centimeters IPS display, offering a crisp 3.2K resolution (3200x2000), 300 nits brightness, and a 16:10 aspect ratio. Enjoy smooth navigation and fluid graphics with a 90Hz refresh rate, while the anti-glare and TUV Low Blue Light technology ensure comfortable viewing for extended periods. Equipped with 16GB DDR4 3200 MHz RAM and a lightning-fast 512GB PCIe NVMe M.2 SSD, multitasking and storage are a breeze. Powered by Windows 11 Home and bundled with Microsoft Office Home and Student 2021, this laptop offers an unparalleled computing experience for work, study, and entertainment.",
+    price: "49999",
+  },
+  {
+    id: 7,
+    category: "tablets",
+    title: "Xiaomi Pad 6",
+    imgSrc: "https://m.media-amazon.com/images/I/71LRY1j6UHL._SX679_.jpg",
+    amazonLink: "https://amzn.to/3ZeNRdY",
+    description:
+      "Xiaomi Pad 6| Qualcomm Snapdragon 870| 144Hz Refresh Rate| 6GB, 128GB| 2.8K+ Display",
+    brief_det:
+      "Unleash unparalleled performance and immersive entertainment with the latest smartphone featuring a Snapdragon 870 Octa-Core Processor paired with Adreno 650 GPU and Qualcomm AI engine, ensuring seamless multitasking and gaming experiences. Experience fluid visuals on the stunning 27.9cm(11) display with a 144Hz 7-Stage Refresh Rate and 2.8K Resolution, boasting 1 Billion Colors and 2880*1800 High Resolution for lifelike clarity and vibrancy. Dive into cinematic soundscapes with Quad Speakers powered by Dolby Atmos, while the long-lasting 8840mAh battery ensures uninterrupted usage throughout the day. Stay updated and secure with Android 13 and MIUI 14, delivering the latest features and security updates. Capture stunning selfies with the 8MP Front Camera featuring Focus Frame, and crystal-clear photos with the 13MP Rear Camera. With its sleek Metal Unibody Design, this smartphone combines style with durability for a premium experience.",
+    price: "26999",
+  },
+  {
+    id: 2,
+    category: "mobiles",
+    title: "Apple iPhone 12",
+    imgSrc: "https://m.media-amazon.com/images/I/711wsjBtWeL._SX679_.jpg",
+    amazonLink: "https://amzn.to/44MiYi6",
+    description: "Apple iPhone 12 (64GB) - White",
+    brief_det:
+      "Introducing the latest smartphone with a 6.1-inch Super Retina XDR display and Ceramic Shield for ultimate durability. Powered by the A14 Bionic chip, it boasts lightning-fast performance. Capture stunning photos and videos with its advanced dual-camera system and 12MP TrueDepth front camera, featuring Night mode and 4K Dolby Vision HDR recording. With industry-leading IP68 water resistance, this smartphone is designed to withstand any adventure.",
+    price: "44999",
+  },
+  {
+    id: 6,
+    category: "tablets",
+    title: "Lenovo Tab P12 Pro AMOLED",
+    imgSrc: "https://m.media-amazon.com/images/I/61ps0pf7SrL._SX679_.jpg",
+    amazonLink: "https://amzn.to/3RabsdH",
+    description:
+      "Lenovo Tab P12 Pro AMOLED (12.6 inch (32 cm), 8 GB, 256 GB, Wi-fi Only), Storm Grey with Precision Pen 3",
+    brief_det:
+      "Introducing the latest tablet featuring a stunning 12.7-inch 3K display with 400 nits brightness and 1 billion color depth for an immersive viewing experience. Enjoy crystal-clear audio with quad JBL speakers optimized with Dolby Atmos technology. With 8GB of RAM and 128GB of storage, expandable up to 1TB via microSD card, this tablet offers ample space for all your needs. Powered by a Mediatek Dimensity Octa-Core Processor, it delivers smooth performance for multitasking and entertainment. Stay connected with Wi-Fi 6, Bluetooth 5.1, and USB Type-C connectivity, all powered by Android OS 13 for a seamless user experience.",
+    price: "26999",
+  },
+  {
+    id: 3,
+    category: "mobiles",
+    title: "Apple iPhone 13",
+    imgSrc: "https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg",
+    amazonLink: "https://amzn.to/3r6XJtv",
+    description: "Apple iPhone 13 (128GB) - Blue",
+    brief_det:
+      "Introducing the latest smartphone featuring a 6.1-inch Super Retina XDR display for stunning visuals. Capture cinematic videos with automatic focus shifts using Cinematic mode, along with advanced dual-camera system and 12MP TrueDepth front camera for exceptional photography. Powered by the A15 Bionic chip, it delivers lightning-fast performance. Experience immersive entertainment and seamless multitasking with this cutting-edge device.",
+    price: "51999",
+  },
+  {
+    id: 4,
+    category: "laptops",
+    title: "Lenovo IdeaPad Slim 5 ",
+    imgSrc: "https://m.media-amazon.com/images/I/61OtrBpfvIL._SX679_.jpg",
+    amazonLink: "https://amzn.to/44JsK4r",
+    description: 'Lenovo IdeaPad Slim 5 Intel Core i5 12450H 14" (35.5cm) ',
+    brief_det:
+      "Introducing the latest laptop powered by an Intel Core i5-12450H processor, offering speeds up to 4.4 GHz for seamless performance. Pre-loaded with Windows 11 Home and MS Office Home and Student 2021, it comes with Xbox GamePass Ultimate 3-month subscription. Enjoy smooth multitasking with 16GB LPDDR5-5200 RAM and 512GB SSD, expandable up to 1TB. Featuring a vibrant 16 WUXGA IPS display with 300 nits brightness and anti-glare technology. Sleek and lightweight design, backlit keyboard, and long-lasting battery with Rapid Charge technology ensure enhanced productivity on the go.",
+    price: "59999",
+  },
+  {
+    id: 9,
+    category: "tablets",
+    title: "Samsung Galaxy Tab S9 FE",
+    imgSrc: "https://m.media-amazon.com/images/I/61M4gkurLUL._SX679_.jpg",
+    amazonLink: "https://amzn.to/3Pdsh4F",
+    description:
+      "Samsung Galaxy Tab S9 FE 27.69 cm (10.9 inch) Display, RAM 6 GB, ROM 128 GB Expandable, S Pen in-Box, Wi-Fi, IP68 Tablet, Gray",
+    brief_det:
+      "Experience unparalleled vividness and clarity on the expansive 27.69 cm (10.9) display of our latest tablet, boasting a high refresh rate of 90 Hz and a resolution of 2304 x 1440 (WQXGA) with 249 PPI. Powered by the formidable Exynos 1380 chip, this tablet delivers powerful performance for seamless multitasking and smooth user experience. Capture stunning photos and videos with the 8 MP rear camera and 12 MP ultra-wide front camera, complemented by dual speakers tuned by AKG for immersive audio. With an impressive 8000 mAh battery and support for dual SIM (pSIM + eSIM), stay connected and productive on the go. Designed to withstand the elements, both the tablet and included S Pen feature IP68 weatherproof and durability ratings, ensuring peace of mind wherever your adventures take you.",
+    price: "36999",
+  },
+  {
+    id: 8,
+    category: "laptops",
+    title: "Apple MacBook Air M1 Laptop",
+    imgSrc: "https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg",
+    amazonLink: "https://amzn.to/3r9rvhm",
+    description:
+      "Apple MacBook Air Laptop M1 chip, 13.3-inch/33.74 cm Retina Display, ",
+    brief_det:
+      "Experience unparalleled performance and endurance with the latest MacBook Air. Its all-day battery life ensures you can keep going for up to 18 hours without interruption. Powered by the revolutionary Apple M1 chip featuring an 8-core CPU, it delivers an astounding 3.5x faster performance while consuming significantly less power. With 8GB of superfast unified memory, tasks are executed swiftly and efficiently, from memory-intensive multitab browsing to handling large graphic files effortlessly. Immerse yourself in the stunning 13.3-inch Retina display, where images burst with realism, text is razor-sharp, and colors are vivid, elevating your viewing experience to new heights.",
+    price: "75999",
+  },
+];
